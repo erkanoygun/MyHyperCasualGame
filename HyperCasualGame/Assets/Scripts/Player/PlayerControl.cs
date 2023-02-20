042a0c8f1cbfcea777c106a282b2dde827fd9c12
Mücahit Erkan Oygun <erkanoygun@gmail.com>
2023-02-20 19:39:21 +0300

    Charakter model, run and jump animation added, player smooth transition to the right and left..

--- HyperCasualGame/Assets/Scripts/Player/PlayerControl.cs
+++ HyperCasualGame/Assets/Scripts/Player/PlayerControl.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
+using System;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
@@ -9,29 +8,34 @@ public class PlayerControl : MonoBehaviour
     UnityEngine.Vector2 firstPressPos;
     UnityEngine.Vector2 secondPressPos;
     UnityEngine.Vector2 currentSwipe;
-    private float posX, posY, posZ;
-    [SerializeField] private float _speed = 10.0f;
+    [SerializeField] private float _speed = 5.0f;
+    Animator _animator;
+    [SerializeField] private GameObject _playerBody;
+
     void Start()
     {
-        /*posX = transform.position.x;
-        posY = transform.position.y;
-        posZ = transform.position.z;*/
+        _animator = _playerBody.GetComponent<Animator>();     
         _rb = GetComponent<Rigidbody>();
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _animator.SetBool("isJump", false);
+        }
+    }
+
+
     void Update()
     {
+        
         Swipe();
     }
 
     void FixedUpdate()
     {
-        //UnityEngine.Vector3 vector = new UnityEngine.Vector3(0,0,1);
-        //_rb.AddForce(vector * _speed, ForceMode.Force);
-
-        //transform.position = new UnityEngine.Vector3(posX, transform.position.y, posZ);
-        transform.position += transform.forward * 2 * Time.fixedDeltaTime;
+        transform.position += transform.forward * _speed * Time.fixedDeltaTime;
     }
 
     public void Swipe()
@@ -39,37 +43,40 @@ public class PlayerControl : MonoBehaviour
         // first touch screen
         if (Input.GetMouseButtonDown(0))
         {
-            firstPressPos = new UnityEngine.Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }
         // ended touch screen
         if (Input.GetMouseButtonUp(0))
         {
-            secondPressPos = new UnityEngine.Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
             //create vector from the two points
-            currentSwipe = new UnityEngine.Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
 
             currentSwipe.Normalize();
 
             // Up swipe
             if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
             {
-                transform.position += transform.up * 3;
+
+                _rb.velocity = new Vector3(0, 8, 0);
+                _animator.SetBool("isJump", true);
+                //transform.position += transform.up * 3;
             }
 
             //Left swipe
             if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
             {
-                Debug.Log("left swipe");
-                transform.position += (transform.right * -1) * 2;
+                _rb.velocity = new Vector3(-1f * SwipeXForce((secondPressPos.x - firstPressPos.x)), 0, 0);
+                //transform.position += (transform.right * -1) * 2;
                 
             }
 
             //Right swipe
             if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
             {
-                Debug.Log("right swipe");
-                transform.position += transform.right * 2;
+                _rb.velocity = new Vector3(SwipeXForce((secondPressPos.x - firstPressPos.x)), 0, 0);
+                //transform.position += transform.right * 2;
             }
             /*
             //Down Swipe
@@ -78,5 +85,36 @@ public class PlayerControl : MonoBehaviour
                 Debug.Log("down swipe");
             }*/
         }
+    }
+
+    private float SwipeXForce(float value)
+    {
+        float force;
+        value = Math.Abs(value);
+        if(value >= 0 && value <= 5)
+        {
+            force = 1.2f;
+        }
+        else if (value >= 6 && value <= 20)
+        {
+            force = 3.2f;
+        }
+        else if (value >= 21 && value <= 90)
+        {
+            force = 4.2f;
+        }
+        else if (value >= 91 && value <= 200)
+        {
+            force = 9.2f;
+        }
+        else if (value >= 201 && value <= 350)
+        {
+            force = 11.2f;
+        }
+        else
+        {
+            force = 14.2f;
+        }
+        return force;
     }
 }
